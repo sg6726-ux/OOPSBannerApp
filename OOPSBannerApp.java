9845c1971c1ab3b3ccd1cab8a3309c2e7b69d028
sg6726-ux <sg6726@srmist.edu.in>
2026-02-24 01:30:29 +0530

    UC4 Arrays & Loops

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,83 +1,19 @@
-import java.util.Scanner;
-
 public class OOPSBannerApp {
 
     public static void main(String[] args) {
 
-        Scanner sc = new Scanner(System.in);
+        String[] banner = {
+                " *****   *****   *****   ***** ",
+                "*     * *     * *     * *      ",
+                "*     * *     * *     * *      ",
+                "*     * *     * *****   *****  ",
+                "*     * *     * *           *  ",
+                "*     * *     * *           *  ",
+                " *****   *****  *       *****  "
+        };
 
-        System.out.print("Enter Text: ");
-        String text = sc.nextLine().toUpperCase();
-
-        for (int row = 0; row < 7; row++) {
-
-            for (int i = 0; i < text.length(); i++) {
-
-                char ch = text.charAt(i);
-
-                switch (ch) {
-
-                    case 'O':
-                        printO(row);
-                        break;
-
-                    case 'P':
-                        printP(row);
-                        break;
-
-                    case 'S':
-                        printS(row);
-                        break;
-
-                    default:
-                        System.out.print("       ");
-                }
-
-                System.out.print("  ");
-            }
-
-            System.out.println();
-        }
-
-        sc.close();
-    }
-
-    // ---------- LETTER O ----------
-    static void printO(int row) {
-        switch(row){
-            case 0: case 6:
-                System.out.print(" ***** ");
-                break;
-            default:
-                System.out.print("*     *");
-        }
-    }
-
-    // ---------- LETTER P ----------
-    static void printP(int row) {
-        switch(row){
-            case 0: case 3:
-                System.out.print(" ***** ");
-                break;
-            case 1: case 2:
-                System.out.print("*     *");
-                break;
-            default:
-                System.out.print("*      ");
-        }
-    }
-
-    // ---------- LETTER S ----------
-    static void printS(int row) {
-        switch(row){
-            case 0: case 3: case 6:
-                System.out.print(" ***** ");
-                break;
-            case 1: case 2:
-                System.out.print("*      ");
-                break;
-            default:
-                System.out.print("      *");
+        for(String line : banner){
+            System.out.println(line);
         }
     }
 }
