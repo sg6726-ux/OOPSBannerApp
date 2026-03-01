5d81b5cb940fe069d509f885660469046319f49c
sg6726-ux <sg6726@srmist.edu.in>
2026-03-02 01:21:00 +0530

    UC7 Character Pattern Class

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,52 +1,89 @@
 public class OOPSBannerApp {
 
-    // Method to create O pattern
-    public static String[] getOPattern() {
+    /* ---------- INNER STATIC CLASS ---------- */
+    static class CharacterPattern {
+
+        private char character;
+        private String[] pattern;
+
+        // Constructor
+        public CharacterPattern(char character, String[] pattern) {
+            this.character = character;
+            this.pattern = pattern;
+        }
+
+        // Getter methods
+        public char getCharacter() {
+            return character;
+        }
+
+        public String[] getPattern() {
+            return pattern;
+        }
+    }
+
+    /* ---------- PATTERN METHODS ---------- */
+
+    public static String[] createOPattern() {
         return new String[]{
-            " ***** ",
-            "*     *",
-            "*     *",
-            "*     *",
-            "*     *",
-            "*     *",
-            " ***** "
+                " ***** ",
+                "*     *",
+                "*     *",
+                "*     *",
+                "*     *",
+                "*     *",
+                " ***** "
         };
     }
 
-    // Method to create P pattern
-    public static String[] getPPattern() {
+    public static String[] createPPattern() {
         return new String[]{
-            " ***** ",
-            "*     *",
-            "*     *",
-            " ***** ",
-            "*      ",
-            "*      ",
-            "*      "
+                " ***** ",
+                "*     *",
+                "*     *",
+                " ***** ",
+                "*      ",
+                "*      ",
+                "*      "
         };
     }
 
-    // Method to create S pattern
-    public static String[] getSPattern() {
+    public static String[] createSPattern() {
         return new String[]{
-            " ***** ",
-            "*      ",
-            "*      ",
-            " ***** ",
-            "      *",
-            "      *",
-            " ***** "
+                " ***** ",
+                "*      ",
+                "*      ",
+                " ***** ",
+                "      *",
+                "      *",
+                " ***** "
         };
     }
+
+    /* ---------- MAIN METHOD ---------- */
 
     public static void main(String[] args) {
 
-        String[] O = getOPattern();
-        String[] P = getPPattern();
-        String[] S = getSPattern();
+        CharacterPattern O =
+                new CharacterPattern('O', createOPattern());
+
+        CharacterPattern P =
+                new CharacterPattern('P', createPPattern());
+
+        CharacterPattern S =
+                new CharacterPattern('S', createSPattern());
+
+        CharacterPattern[] word = {O, O, P, S};
 
         for(int i = 0; i < 7; i++) {
-            System.out.println(O[i] + " " + O[i] + " " + P[i] + " " + S[i]);
+
+            StringBuilder line = new StringBuilder();
+
+            for(CharacterPattern cp : word) {
+                line.append(cp.getPattern()[i]).append(" ");
+            }
+
+            System.out.println(line);
         }
     }
 }
